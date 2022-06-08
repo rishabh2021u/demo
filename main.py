c3e961e10b02712482519114a9b095f759a14828
rishabh2021u <105239617+rishabh2021u@users.noreply.github.com>
2022-06-08 19:24:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def uplod():
        open('DASH_480.mp4', 'wb').write(requests.get(urlz + '/DASH_360.mp4').content)
     subprocess.call("ffmpeg -i DASH_480.mp4 -i DASH_audio.mp4 -c:v copy -c:a aac output1.mp4 -y", shell=True, stdout=subprocess.PIPE)
     subprocess.call("ffmpeg -i output1.mp4 -vf 'pad=height=ih+30:x=0:y=0:color=black, drawtext=fontfile=/path/to/font.ttf:text='R_Chodi_is_now_bakchodi.org':fontcolor=white:fontsize=24:x=(w-text_w)/2:y=h-th,drawtext=:text='bakchodi.org':fontcolor=#e7e7e7:fontsize=10:x=1:y=(h-text_h)/2:box=1:boxcolor=black@0.5:boxborderw=3:x=w-text_w:y=(h-text_h)/2' -codec:a copy " + id + "video.mp4 -y", shell=True, stdout=subprocess.PIPE)
-    subprocess.call("./rclone copy " + id + "video.mp4 " + "onedrive:public --config /tmp/rclone.conf --ignore-existing && rm " + id + "video.mp4", shell=True, stdout=subprocess.PIPE)
+    subprocess.call("./rclone copy " + id + "video.mp4 " + "onedrive:public --config rclone.conf --ignore-existing && rm " + id + "video.mp4", shell=True, stdout=subprocess.PIPE)
     return jsonify({'url': "https://vid.rishabh.ml/api/raw/?path=/" + id + "video.mp4"});
 
 if __name__ == '__main__':
